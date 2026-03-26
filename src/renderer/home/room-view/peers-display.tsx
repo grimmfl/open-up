7cecb5ff806a3f57e6ad6b149c692819a57b7ec2
grimmfl <flx.grimm@gmail.com>
2026-03-26 21:09:43 +0100

    Lautstärke pro Person einstellbar #19 - WIP

--- src/renderer/home/room-view/peers-display.tsx
+++ src/renderer/home/room-view/peers-display.tsx
@@ -1,15 +1,31 @@
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {RoomContext} from "../../contexts";
 
 export default function PeersDisplay() {
   const {peerNames} = useContext(RoomContext);
+
+  const [peerMenu, setPeerMenu] = useState<string | null>(null);
+
+  function openPeerMenu(peerId: string): void {
+    setPeerMenu(peerId);
+  }
 
   return (
     <table className="table">
       <tbody>
       {Array.from(peerNames.values()).map((peer, index) =>
         <tr key={`peer${index}`}>
-          <td>{peer}</td>
+          <td onClick={() => openPeerMenu(peer)}>
+            {peer}
+            {peer == peerMenu &&
+              <div className="peer-menu d-flex flex-column">
+                <div className="p-2">
+                  <input type="range" className="custom-range mx-2"></input>
+                  100%
+                </div>
+              </div>
+            }
+          </td>
         </tr>
       )}
       </tbody>

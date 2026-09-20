93c575d0ac2c5649a3c63ff6e38233840f243f9f
ncl-fg <felix.grimm@newcodelines.com>
2026-09-20 13:34:22 +0200

    Dark Mode not recognized on some systems #43

--- src/main/main.ts
+++ src/main/main.ts
@@ -26,9 +26,7 @@ let mainWindow: BrowserWindow | null = null;
 ipcMain.on('load-data', async (event) => {
   const data = load();
 
-  if (data?.darkMode != null) {
-    nativeTheme.themeSource = data.darkMode ? 'dark' : 'light';
-  }
+  nativeTheme.themeSource = data?.darkMode ?? nativeTheme.shouldUseDarkColors ? 'dark' : 'light';
 
   if (data != null && data?.darkMode == null) {
     data.darkMode = nativeTheme.shouldUseDarkColors;
